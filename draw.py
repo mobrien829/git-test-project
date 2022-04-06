404c807c781d7d6be004e93a93465e782926e86f
Michael <mlobrien829@gmail.com>
2022-04-06 10:50:17 -0400

    added jetbrains gitignore, changed draw.py

--- draw.py
+++ draw.py
@@ -2,7 +2,10 @@ import turtle
 
 painter = turtle.Turtle()
 
-painter.forward(100)
-painter.left(90)
+for _ in range(4):
+    # can use _ as a placeholder
+    # remember that in range() is upper bound exclusive, so [0,4)
+    painter.forward(100)
+    painter.left(90)
 
 turtle.done()
