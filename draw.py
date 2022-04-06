1d071747bd793e1f3065e7357c0c8ba2be5e7920
Michael <mlobrien829@gmail.com>
2022-04-06 11:29:24 -0400

    modified draw.py

--- draw.py
+++ draw.py
@@ -2,10 +2,10 @@ import turtle
 
 painter = turtle.Turtle()
 
-for _ in range(4):
+for _ in range(18):
     # can use _ as a placeholder
     # remember that in range() is upper bound exclusive, so [0,4)
     painter.forward(100)
-    painter.left(90)
+    painter.left(100)
 
 turtle.done()
